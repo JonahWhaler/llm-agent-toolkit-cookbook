b7a4c50d409e9dac0e8657308c03640af2283f96
JonahWhaler <jonah_mytzuchi@outlook.com>
2025-02-28 16:13:05 +0800

    Gemini examples

---
+++ gemini/chat_completion.py
@@ -0,0 +1,122 @@
+"""
+Gemini Chat Completion Example
+
+This module demonstrates how to use the Gemini  text-to-text API for generating chat
+completions in both single-round and multi-round (interactive) modes.
+
+Attention: Add `GEMINI_API_KEY` to .env file.
+"""
+
+import logging
+from dotenv import load_dotenv
+from llm_agent_toolkit import ChatCompletionConfig
+from llm_agent_toolkit.core.gemini import GeminiCore, Text_to_Text
+
+logging.basicConfig(
+    filename="./logs/example-gemini.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+
+def generate(llm: Text_to_Text, prompt: str) -> str | None:
+    """
+    Generate a single chat completion response.
+
+    This function takes an instance of a text-to-text model and a user prompt,
+    sends the prompt to the model, and returns the generated response as a string.
+    In case of runtime errors or exceptions, the error is logged and the function
+    returns None.
+
+    Args:
+        llm (Text_to_Text): An instance configured for text-to-text interactions.
+        prompt (str): The input query or message from the user.
+
+    Returns:
+        str | None: The concatenated response from the model, or None if an error occurs.
+    """
+    try:
+        responses = llm.run(query=prompt, context=None)
+        output_strings = ["\nAssistant:"]
+        for response in responses:
+            output_strings.append(response["content"])
+
+        output_string = "\n".join(output_strings)
+        logger.info(output_string)
+        return "Assistant:" + responses[-1]["content"]
+    except RuntimeError as re:
+        logger.error(str(re), exc_info=True)
+    except Exception as e:
+        logger.error(str(e), exc_info=True)
+    return None
+
+
+def multiround(llm: Text_to_Text, prompt: str | None = None) -> list[dict] | None:
+    """
+    Engage in a multi-round chat conversation with the assistant.
+
+    This function starts an interactive session where the user can have a continuous
+    conversation with the AI assistant. The conversation context is maintained across
+    exchanges, allowing the assistant to reference previous messages. The session
+    terminates when the user inputs "/exit".
+
+    Args:
+        llm (Text_to_Text): An instance of a text-to-text model for interactive conversation.
+        prompt (str | None): Optional initial prompt; if None, the user will be prompted for input.
+
+    Returns:
+        list[dict] | None: A list of conversation turns (each represented as a dictionary)
+                           capturing both user and assistant messages. Returns None if an error occurs.
+    """
+    conversations: list[dict[str, str | dict]] = []
+
+    if prompt is None:
+        prompt = input("User: ")
+
+    try:
+        while prompt != "/exit":
+            responses = llm.run(
+                query=prompt, context=conversations if len(conversations) > 0 else None  # type: ignore
+            )
+            output_strings = ["\nAssistant:"]
+            for response in responses:
+                output_strings.append(response["content"])
+
+            output_string = "\n".join(output_strings)
+            logger.info(output_string)
+            conversations.append({"role": "user", "content": prompt})
+            conversations.extend(responses)  # type: ignore
+            print("AI:\n" + responses[-1]["content"] + "\n")
+            prompt = input("User: ")
+
+    except Exception as e:
+        logger.error(str(e), exc_info=True)
+    return conversations
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    logger = logging.getLogger(__name__)
+    CONFIG = ChatCompletionConfig(
+        name="gemini-1.5-flash",
+        return_n=1,
+        max_iteration=1,
+        max_tokens=2048,
+        max_output_tokens=1024,
+        temperature=0.7,
+    )
+    # Load configuration for the Gemini API from CSV
+    GeminiCore.load_csv("./config/gemini.csv")
+    llm = Text_to_Text(
+        system_prompt="You are a helpful assistant.",
+        config=CONFIG,
+    )
+    # Single-round conversation example
+    PROMPT = "Give me a novel insight on the color of wind."
+    answer = generate(llm, PROMPT)
+    if answer:
+        print(answer)
+
+    # Multi-round conversation example
+    conversations = multiround(llm, None)
---
+++ gemini/image_interpretation.py
@@ -0,0 +1,76 @@
+"""
+Gemini Image Interpretation Example
+
+This module demonstrates how to use the Gemini image-to-text API for image interpretation.
+It shows how to send an image file along with a prompt to the API and receive a textual description
+or analysis of the image.
+
+Attention: Add `GEMINI_API_KEY` to .env file.
+"""
+
+import logging
+from dotenv import load_dotenv
+from llm_agent_toolkit import ChatCompletionConfig
+from llm_agent_toolkit.core.gemini import GeminiCore, Image_to_Text
+
+logging.basicConfig(
+    filename="./logs/example-gemini.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+
+def interpret(llm: Image_to_Text, prompt: str, filepath: str) -> str | None:
+    """
+    Interpret an image using the Gemini image-to-text API.
+
+    This function sends a prompt along with the image specified by the filepath to the API.
+    It processes the API response to extract and log the textual interpretation of the image.
+
+    Args:
+        llm (Image_to_Text): An instance configured for image-to-text interactions.
+        prompt (str): The prompt guiding the interpretation (e.g., "Describe this image.").
+        filepath (str): Path to the image file to be interpreted.
+
+    Returns:
+        str | None: The final interpreted text prefixed with "Assistant:", or None if an error occurs.
+    """
+    try:
+        responses = llm.interpret(query=prompt, context=None, filepath=filepath)
+        output_strings = ["\nAssistant:"]
+        for response in responses:
+            output_strings.append(response["content"])
+
+        output_string = "\n".join(output_strings)
+        logger.info(output_string)
+        return "Assistant:" + responses[-1]["content"]
+    except RuntimeError as re:
+        logger.error(str(re), exc_info=True)
+    except Exception as e:
+        logger.error(str(e), exc_info=True)
+    return None
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    logger = logging.getLogger(__name__)
+    CONFIG = ChatCompletionConfig(
+        name="gemini-1.5-flash",
+        return_n=1,
+        max_iteration=1,
+        max_tokens=2048,
+        max_output_tokens=1024,
+        temperature=0.7,
+    )
+    # Load configuration for the Gemini API from CSV
+    GeminiCore.load_csv("./config/gemini.csv")
+
+    llm = Image_to_Text(system_prompt="You are a helpful assistant.", config=CONFIG)
+
+    PROMPT = "Describe this image."
+    FILEPATH = "./assets/author.jpeg"
+
+    answer = interpret(llm, PROMPT, FILEPATH)
+    if answer:
+        print(answer)
